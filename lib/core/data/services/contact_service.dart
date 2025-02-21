e3792b4ae417e2f2a0a48209157f38abde2d8867
k1ycee <chiagozieani145@gmail.com>
2025-02-21 10:46:35 +0100

    Fetch Single Contact

--- lib/core/data/services/contact_service.dart
+++ lib/core/data/services/contact_service.dart
@@ -4,23 +4,50 @@ import 'package:stash_android/core/data/model/contact_model.dart';
 class ContactService {
   static const platform = MethodChannel('com.devcrazelu.stash/contact');
 
-  static Future<void> updateContact({required ContactModel contactModel}) async {
-    await platform.invokeMethod('updateContact', contactModel.toMap());
+  static Future<void> updateContact(
+      {required ContactModel contactModel}) async {
+    try {
+      await platform.invokeMethod('updateContact', contactModel.toMap());
+    } catch (e) {
+      throw Exception(e.toString());
+    }
   }
 
   static Future<void> deleteContact(int id) async {
-    await platform.invokeMethod('deleteContact', {'id': id});
+    try {
+      await platform.invokeMethod('deleteContact', {'id': id});
+    } catch (e) {
+      throw Exception(e.toString());
+    }
   }
 
-  static Future<void> insertContact({required ContactModel contactModel}) async {
-    await platform.invokeMethod('insertContact', contactModel.toMap());
+  static Future<void> insertContact(
+      {required ContactModel contactModel}) async {
+    try {
+      await platform.invokeMethod('insertContact', contactModel.toMap());
+    } catch (e) {
+      throw Exception(e.toString());
+    }
   }
 
   static Future<List<ContactModel>> getContacts() async {
-    final List<dynamic> users = await platform.invokeMethod('getContacts');
-    return users
-        .map((user) =>
-            ContactModel.fromMap(Map<String, dynamic>.from(user as Map)))
-        .toList();
+    try {
+      final List<dynamic> contacts = await platform.invokeMethod('getContacts');
+      return contacts
+          .map((contact) =>
+              ContactModel.fromMap(Map<String, dynamic>.from(contact as Map)))
+          .toList();
+    } catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  static Future<ContactModel> getContact(int id) async {
+    try {
+      final contact = await platform.invokeMethod('fetchContact', {'id': id});
+      return ContactModel.fromMap(Map<String, dynamic>.from(contact as Map));
+    } catch (e) {
+      throw Exception(e.toString());
+    }
   }
 }
--- lib/features/initialization/presentation/screens/contact/contact_home.dart
+++ lib/features/initialization/presentation/screens/contact/contact_home.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:gap/gap.dart';
 import 'package:provider/provider.dart';
+import 'package:stash_android/core/data/services/contact_service.dart';
 import 'package:stash_android/core/presentation/viewmodel/view_model_provider.dart';
 import 'package:stash_android/features/initialization/presentation/viewmodels/contact_view_model.dart';
 
@@ -12,6 +12,7 @@ class ContactHomeScreen extends StatefulWidget {
 }
 
 class _ContactHomeScreenState extends State<ContactHomeScreen> {
+  int selectedMenu = 0;
   @override
   Widget build(BuildContext context) {
     return ViewModelProvider(
@@ -19,66 +20,74 @@ class _ContactHomeScreenState extends State<ContactHomeScreen> {
       builder: (context) {
         final viewModel = context.read<ContactViewModel>();
         return Scaffold(
+          appBar: AppBar(
+            leading: Text(
+              'Stash',
+              style: Theme.of(context).textTheme.headlineLarge,
+            ),
+            leadingWidth: 90,
+          ),
           floatingActionButton: FloatingActionButton(
-            // onPressed: () {},
             onPressed: viewModel.fetchContacts,
             child: const Icon(Icons.add),
           ),
-          body: Column(
-            children: [
-              const Gap(100),
-              Row(
-                children: [
-                  Text(
-                    'Stash',
-                    style: Theme.of(context).textTheme.headlineLarge,
-                  ),
-                ],
-              ),
-              ValueListenableBuilder(
-                valueListenable: viewModel.contacts,
-                builder: (context, contacts, _) {
-                  if (contacts.isEmpty) {
-                    return const Center(
-                      child: Column(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        crossAxisAlignment: CrossAxisAlignment.center,
-                        children: [Text("It's super quiet in here 🙈")],
-                      ),
-                    );
-                  } else {
-                    return Flexible(
-                      child: ListView.builder(
-                        itemCount: contacts.length,
-                        itemBuilder: (context, index) {
-                          return ListTile(
-                            leading: Container(
-                              height: 50,
-                              width: 50,
-                              decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: Colors.grey[600],
-                              ),
-                              child: Center(
-                                child: Text(
-                                  contacts[index].firstName?.substring(0,1) ?? '',
-                                  style: Theme.of(context).textTheme.bodyLarge,
+          body: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ValueListenableBuilder(
+                  valueListenable: viewModel.contacts,
+                  builder: (context, contacts, _) {
+                    if (contacts.isEmpty) {
+                      return const Center(
+                        child: Text("It's super quiet in here 🙈"),
+                      );
+                    } else {
+                      return Flexible(
+                        child: ListView.builder(
+                          itemCount: contacts.length,
+                          itemBuilder: (context, index) {
+                            return ListTile(
+                              leading: Container(
+                                height: 50,
+                                width: 50,
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.grey[600],
+                                ),
+                                child: Center(
+                                  child: Text(
+                                    contacts[index]
+                                            .firstName
+                                            ?.substring(0, 1) ??
+                                        '',
+                                    style:
+                                        Theme.of(context).textTheme.bodyLarge,
+                                  ),
                                 ),
                               ),
-                            ),
-                            title: Text(
-                              contacts[index].firstName ?? '',
-                              style: Theme.of(context).textTheme.bodyLarge,
-                            ),
-                            trailing: const Icon(Icons.info_outline, color: Colors.blue,),
-                          );
-                        },
-                      ),
-                    );
-                  }
-                },
-              )
-            ],
+                              onTap: () {
+                                ContactService.getContact(
+                                    contacts[index].id ?? 0);
+                              },
+                              title: Text(
+                                contacts[index].firstName ?? '',
+                                style: Theme.of(context).textTheme.bodyLarge,
+                              ),
+                              trailing: const Icon(
+                                Icons.info_outline,
+                                color: Colors.blue,
+                              ),
+                            );
+                          },
+                        ),
+                      );
+                    }
+                  },
+                )
+              ],
+            ),
           ),
         );
       },
