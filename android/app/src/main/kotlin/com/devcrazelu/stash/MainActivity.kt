915c5059beae4586a3b02bde96044739fefdd8c2
k1ycee <chiagozieani145@gmail.com>
2025-03-02 22:25:25 +0100

    Updated the kotlin code for readability, deleted tests

--- android/app/src/main/kotlin/com/devcrazelu/stash/MainActivity.kt
+++ android/app/src/main/kotlin/com/devcrazelu/stash/MainActivity.kt
@@ -2,6 +2,7 @@ package com.devcrazelu.stash
 
 import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -11,93 +12,112 @@ class MainActivity : FlutterActivity() {
 
     private val CONTACT_CHANNEL = "com.devcrazelu.stash/contact"
 
+
+    private lateinit var database: AppDatabase
+    private lateinit var contactDao: ContactDao
+
+
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
-        val database = AppDatabase.getDatabase(this)
-        val contactDao = database.contactDao()
+        
+    
+        database = AppDatabase.getDatabase(applicationContext)
+        contactDao = database.contactDao()
 
         MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CONTACT_CHANNEL)
                 .setMethodCallHandler { call, result ->
                     when (call.method) {
-                        "insertContact" -> {
-                            val firstName = call.argument<String>("firstName")
-                            val lastName = call.argument<String>("lastName")
-                            val number = call.argument<String>("number")
-                            if (firstName != null && lastName != null && number != null) {
-                                CoroutineScope(Dispatchers.IO).launch {
-                                    contactDao.insert(
-                                            ContactModel(
-                                                    firstName = firstName,
-                                                    lastName = lastName,
-                                                    number = number
-                                            )
-                                    )
-                                    result.success("Contact saved")
-                                }
-                            } else {
-                                result.error("INVALID_ARGUMENTS", "Invalid name or number", null)
-                            }
-                        }
-                        "getContacts" -> {
-                            CoroutineScope(Dispatchers.IO).launch {
-                                val users = contactDao.getAllContacts()
-                                result.success(
-                                        users.map {
-                                            mapOf(
-                                                    "id" to it.id,
-                                                    "firstName" to it.firstName,
-                                                    "lastName" to it.lastName,
-                                                    "number" to it.number
-                                            )
-                                        }
-                                )
-                            }
-                        }
-                        "updateContact" -> {
-                            val id = call.argument<Int>("id")
-                            if (id != null) {
-                                val firstName = call.argument<String>("firstName")
-                                val lastName = call.argument<String>("lastName")
-                                val number = call.argument<String>("number")
-                                if (firstName != null && lastName != null && number != null) {
-                                    CoroutineScope(Dispatchers.IO).launch {
-                                        contactDao.update(
-                                                ContactModel(
-                                                        id = id,
-                                                        firstName = firstName,
-                                                        lastName = lastName,
-                                                        number = number
-                                                )
-                                        )
-                                        result.success("Contact Updated")
-                                    }
-                                } else {
-                                    result.error(
-                                            "INVALID_ARGUMENTS",
-                                            "Invalid name or number",
-                                            null
-                                    )
-                                }
-                            }
-                        }
-                        "deleteContact" -> {
-                            val id = call.argument<Int>("id")
-                            if (id != null) {
-                                CoroutineScope(Dispatchers.IO).launch {
-                                    val user = contactDao.getAllContacts().find { it.id == id }
-                                    if (user != null) {
-                                        contactDao.delete(user)
-                                        result.success("User Deleted")
-                                    } else {
-                                        result.error("USER_NOT_FOUND", "User not found", null)
-                                    }
-                                }
-                            } else {
-                                result.error("INVALID_ARGUMENTS", "Invalid ID", null)
-                            }
-                        }
+                        "insertContact" -> insertContact(call, result)
+                        "getContacts" -> getContacts(result)
+                        "updateContact" -> updateContact(call, result)
+                        "deleteContact" -> deleteContact(call, result)
                         else -> result.notImplemented()
                     }
                 }
+    }
+
+
+    private fun insertContact(call: MethodCall, result: MethodChannel.Result){
+        val firstName = call.argument<String>("firstName")
+        val lastName = call.argument<String>("lastName")
+        val number = call.argument<String>("number")
+        if (firstName != null && lastName != null && number != null) {
+            CoroutineScope(Dispatchers.IO).launch {
+                contactDao.insert(
+                    ContactModel(
+                        firstName = firstName,
+                        lastName = lastName,
+                        number = number
+                    )
+                )
+                result.success("Contact saved")
+            }
+        } else {
+            result.error("INVALID_ARGUMENTS", "Invalid name or number", null)
+        }
+    }
+
+
+    private fun getContacts(result: MethodChannel.Result){
+        CoroutineScope(Dispatchers.IO).launch {
+            val users = contactDao.getAllContacts()
+            result.success(
+                users.map {
+                    mapOf(
+                        "id" to it.id,
+                        "firstName" to it.firstName,
+                        "lastName" to it.lastName,
+                        "number" to it.number
+                    )
+                }
+            )
+        }
+    }
+
+    private fun updateContact(call: MethodCall, result: MethodChannel.Result){
+        val id = call.argument<Int>("id")
+        if (id != null) {
+            val firstName = call.argument<String>("firstName")
+            val lastName = call.argument<String>("lastName")
+            val number = call.argument<String>("number")
+            if (firstName != null && lastName != null && number != null) {
+                CoroutineScope(Dispatchers.IO).launch {
+                    contactDao.update(
+                        ContactModel(
+                            id = id,
+                            firstName = firstName,
+                            lastName = lastName,
+                            number = number
+                        )
+                    )
+                    result.success("Contact Updated")
+                }
+            } else {
+                result.error(
+                    "INVALID_ARGUMENTS",
+                    "Invalid name or number",
+                    null
+                )
+            }
+
+        }
+    }
+
+    private fun deleteContact(call: MethodCall, result: MethodChannel.Result){
+        val id = call.argument<Int>("id")
+        if (id != null) {
+            CoroutineScope(Dispatchers.IO).launch {
+                val users = contactDao.getAllContacts()
+                val user = users.find { it.id == id }
+                if (user != null) {
+                    contactDao.delete(user)
+                    result.success("User Deleted")
+                } else {
+                    result.error("USER_NOT_FOUND", "User not found", null)
+                }
+            }
+        } else {
+            result.error("INVALID_ARGUMENTS", "Invalid ID", null)
+        }
     }
 }
--- test/mocks.dart
+++
@@ -1,9 +0,0 @@
-import 'package:mockito/annotations.dart';
-import 'package:stash_android/core/dialog/dialog_handler.dart';
-import 'package:stash_android/core/navigation/navigation_bus.dart';
-
-@GenerateNiceMocks([
-    MockSpec<DialogHandler>(),
-    MockSpec<NavigationBus>(),
-    ])
-void main() {}
